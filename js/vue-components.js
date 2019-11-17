4004b9869df590e2d3d0c6baeb6dae15871998ef
anton-sergeenkov <warnes23@yandex.ru>
2019-11-17 13:59:22 +0300

    z

--- js/vue-components.js
+++ js/vue-components.js
@@ -81,3 +81,18 @@ Vue.component('v-method-return', {
 			<span class="el text">- <slot></slot></span>
 		</div>`,
 })
+
+Vue.component('v-iframe', {
+	props: ['height', 'src'],
+	template:
+		`<iframe
+			:src="src"
+			:height="height"
+			style="width: 100%;"
+			scrolling="no"
+			frameborder="no"
+			allowtransparency="true"
+			allowfullscreen="true"
+		>
+		</iframe>`,
+})
