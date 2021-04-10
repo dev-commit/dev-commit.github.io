089ed402974acb733081cbbb960407b5227d11b5
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-04-11 00:46:12 +0300

    bash script

--- @modules/build-node-js.js
+++ @modules/build-node-js.js
@@ -3,30 +3,44 @@ ARR_MODULE = [
         title: 'Основное',
         themes: [
             ['node-js/main/info',     emoji.theory+'Информация'],
-            ['node-js/main/function', emoji.pizza+'Функции'],
-            ['node-js/main/modules',  emoji.pizza+'Модули'],
             ['node-js/main/files',    emoji.pizza+'Файлы и директории'],
-            ['node-js/main/export',   setTheme(1, ['require', 'exports'])],
             ['node-js/main/pipe',     setTheme(1, ['.pipe'])],
-        
-            ['node-js/main/server',   'Сервер'],
             ['node-js/main/stream',   'Потоки'],
             ['node-js/main/router',   'Маршрутизация'],
             ['node-js/main/url',      'Данные из url'],
         ]
     },
     {
-        title: 'NPM пакеты',
+        title: 'Модульный принцип',
+        themes: [
+            ['node-js/modules/structure', emoji.theory+'Структура модуля'],
+            ['node-js/modules/require',   setTheme(1, ['require', 'module.exports'])],
+        ]
+    },
+    {
+        title: 'Встроенные модули',
+        themes: [
+            ['node-js/own-modules/path',   setTheme(2, ['path'])],
+            ['node-js/own-modules/fs',     setTheme(2, ['fs'])],
+            ['node-js/own-modules/http',   setTheme(2, ['http'])],
+            ['node-js/own-modules/events', setTheme(2, ['events'])],
+            ['node-js/own-modules/util',   setTheme(2, ['util'])],
+        ]
+    },
+    {
+        title: 'Пакеты',
         themes: [
             ['node-js/package/express',     setTheme(2, ['express'])],
             ['node-js/package/body-parser', setTheme(2, ['body-parser'])],
             ['node-js/package/nodemailer',  setTheme(2, ['nodemailer'])],
+            ['node-js/package/nodemon',     setTheme(2, ['nodemon'])],
         ]
     },
     {
         title: 'Прочее',
         themes: [
-            ['node-js/other/samples', 'Примеры'],
+            ['node-js/other/samples',  'Примеры'],
+            ['node-js/other/function', 'Функции'],
         ]
     },
 ]
