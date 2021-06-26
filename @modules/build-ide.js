6f4de2fbc05ed0bcf4909b341d390f0966440f44
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-06-26 12:00:44 +0300

    bash script

--- @modules/build-ide.js
+++ @modules/build-ide.js
@@ -2,19 +2,27 @@ ARR_MODULE = [
     {
         title: 'VSCode',
         themes: [
-            ['ide/vscode/info',  'Информация'],
+            ['ide/vscode/info',              'Информация'],
+            ['ide/vscode/plugins',           emoji.fire+'Плагины'],
+            ['ide/vscode/snippets',          emoji.code+'Сниппеты'],
+            ['ide/vscode/settings',          emoji.code+'Настройки'],
+
+            ['ide/vscode/extension-eslint',   setTheme(1, ['ESLint'])],
+            ['ide/vscode/extension-prettier', setTheme(1, ['Prettier'])],
         ]
     },
     {
-        title: 'Atom',
+        title: 'Редакторы код',
         themes: [
-            ['ide/atom/info',  'Информация'],
+            ['ide/editors/atom',          'Atom'],
+            ['ide/editors/sublime-text',  'Sublime Text'],
+            ['ide/editors/codesandbox',   'CodeSandbox'],
         ]
     },
     {
-        title: 'Sublime Text',
+        title: 'Плагины',
         themes: [
-            ['ide/sublime-text/info',  'Информация'],
+            ['ide/plugins/emmet',  'Emmet'],
         ]
     },
 ];
--- @modules/build-other.js
+++ @modules/build-other.js
@@ -3,14 +3,12 @@ ARR_MODULE = [
         title: 'Технологии',
         themes: [
             ['other/technology/jira',             'Jira'],
-            ['other/technology/emmet',            'Emmet'],
             ['other/technology/react-native',     'React.Native'],
         ]
     },
     {
         title: 'Информация',
         themes: [
-            ['other/main/code-editors',           'Редакторы кода'],
             ['other/main/chrome-mobile-version',  'Chrome: Мобильная версия'],
             ['other/main/server-online',          'Server Online'],
             ['other/main/photoshop',              'Adobe Photoshop'],
