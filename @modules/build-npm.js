15d88226c5b8e9b6bbd52831a6f247fe40e79c76
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-07-11 20:44:59 +0300

    bash script

--- @modules/build-npm.js
+++ @modules/build-npm.js
@@ -2,11 +2,13 @@ ARR_MODULE = [
     {
         title: 'NPM',
         themes: [
-            ['npm/npm/info',         emoji.theory+'Информация'],
-            ['npm/npm/command',      emoji.code+'Команды'],
-
-            ['npm/npm/linux',        'Linux'],
-            ['npm/npm/shortcut',     'Сокращения'],
+            ['npm/npm/info',                emoji.theory+'Информация'],
+            ['npm/npm/command',             emoji.code+'Команды'],
+            ['npm/npm/package-version',     emoji.code+'Установка версии пакета'],
+            ['npm/npm/nvm',                 'Node Version Manager'],
+            ['npm/npm/package-publishing',  'Публикация пакета'],
+            ['npm/npm/linux',               'Linux'],
+            ['npm/npm-package/http-server', setTheme(2, ['http-server'])],
         ]
     },
     {
@@ -17,15 +19,15 @@ ARR_MODULE = [
         ]
     },
     {
-        title: 'NPM Packages',
-        themes: [
-            ['npm/npm-package/http-server', setTheme(2, ['http-server'])],
-        ]
-    },
-    {
         title: 'Yarn',
         themes: [
             ['npm/yarn/main', 'Основное'],
+        ]
+    },
+    {
+        title: 'Терминал',
+        themes: [
+            ['npm/terminal/command', emoji.code+'Команды'],
         ]
     },
 ]
