85673b7d1faaf1a862d138561ba55591c480116e
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-09-23 16:01:20 +0300

    bash script

--- @modules/build-react-api.js
+++ @modules/build-react-api.js
@@ -2,33 +2,33 @@ ARR_MODULE = [
     {
         title: 'React',
         themes: [
-            ['react-api/react/component',        emoji.chevron+setTheme(4, ['React.Component'])],   
-            ['react-api/react/create-element',   emoji.chevron+setTheme(4, ['React.createElement()'])],
-            ['react-api/react/clone-element',    emoji.chevron+setTheme(4, ['React.cloneElement()'])],
-            ['react-api/react/create-factory',   emoji.chevron+setTheme(4, ['React.createFactory'])],
-            ['react-api/react/is-valid-element', emoji.chevron+setTheme(4, ['React.isValidElement()'])],
+            ['react-api/react/component',        setTheme(1, ['React.Component'])],   
+            ['react-api/react/create-element',   setTheme(1, ['React.createElement()'])],
+            ['react-api/react/clone-element',    setTheme(1, ['React.cloneElement()'])],
+            ['react-api/react/create-factory',   setTheme(1, ['React.createFactory'])],
+            ['react-api/react/is-valid-element', setTheme(1, ['React.isValidElement()'])],
         ]
     },
     {
         title: 'Оптимизация',
         themes: [
             ['react-api/optimization/info',                 emoji.theory+'Информация'],
-            ['react-api/optimization/react-pure-component', emoji.chevron+setTheme(4, ['React.PureComponent'])],
-            ['react-api/optimization/react-memo',           emoji.chevron+setTheme(4, ['React.memo'])],
-            ['react-api/optimization/react-lazy',           emoji.chevron+setTheme(4, ['React.lazy', 'React.Suspense'])],
+            ['react-api/optimization/react-pure-component', setTheme(1, ['React.PureComponent'])],
+            ['react-api/optimization/react-memo',           setTheme(1, ['React.memo'])],
+            ['react-api/optimization/react-lazy',           setTheme(1, ['React.lazy', 'React.Suspense'])],
         ]
     },
     {
         title: 'Компоненты',
         themes: [
-            ['react-api/components/set-state',    emoji.chevron+setTheme(4, ['setState()'])],
-            ['react-api/components/force-update', emoji.chevron+setTheme(4, ['forceUpdate()'])],
+            ['react-api/components/set-state',    setTheme(1, ['setState()'])],
+            ['react-api/components/force-update', setTheme(1, ['forceUpdate()'])],
         ]
     },
     {
         title: 'DOM-атрибуты',
         themes: [
-            ['react-api/dom/dangerously-set-inner-html',  emoji.chevron+setTheme(4, ['dangerouslySetInnerHTML'])],
+            ['react-api/dom/dangerously-set-inner-html',  setTheme(1, ['dangerouslySetInnerHTML'])],
         ]
     },
 ]
