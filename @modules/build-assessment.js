8168b53ce35e7606a053446628db73b208957ec4
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2021-11-09 17:12:03 +0300

    bash script

--- @modules/build-assessment.js
+++ @modules/build-assessment.js
@@ -57,7 +57,7 @@ ARR_MODULE = [
             ['theory-paradigms/fp/concept',                      emoji.lemon+'ФП'],
             ['theory-paradigms/best-practices/solid',            emoji.baseball+'SOLID'],
             ['theory-patterns/main/info',                        emoji.theory+'Паттерны'],
-            ['testing/theory/pyramid',                           emoji.theory+'Пирамида тестирования'],
+            ['testing/pyramid/pyramid',                          emoji.theory+'Пирамида тестирования'],
             ['compilation/deployment/info',                      emoji.theory+'CI / CD'],
         ]
     },
--- @modules/build-testing.js
+++ @modules/build-testing.js
@@ -5,13 +5,24 @@ ARR_MODULE = [
         title: 'Теория',
         themes: [
             [root+'theory/info',             emoji.theory+'Тестирование'],
-            [root+'theory/pyramid',          emoji.theory+'Пирамида тестирования'],
-            [root+'theory/unit-test',        emoji.star+'1. Юнит-тесты'],
-            [root+'theory/integration-test', emoji.star+'2. Интеграционные тесты'],
-            [root+'theory/e2e-test',         emoji.star+'3. E2E-тесты'],
-            [root+'theory/tdd',              emoji.avocado+'TDD'],
-            [root+'theory/bdd',              emoji.avocado+'BDD'],
             [root+'theory/types',            emoji.theory+'Разновидности'],
+        ]
+    },
+    {
+        title: 'Пирамида тестирования',
+        themes: [
+            [root+'pyramid/pyramid',          emoji.theory+'Пирамида тестирования'],
+            [root+'pyramid/unit-test',        emoji.star+'1. Юнит-тесты'],
+            [root+'pyramid/integration-test', emoji.star+'2. Интеграционные тесты'],
+            [root+'pyramid/e2e-test',         emoji.star+'3. E2E-тесты'],
+        ]
+    },
+    {
+        title: 'Концепции',
+        themes: [
+            [root+'concepts/tdd',   emoji.icecream+'TDD'],
+            [root+'concepts/bdd',   emoji.icecream+'BDD'],
+            [root+'concepts/first', emoji.icecream+'FIRST'],
         ]
     },
     {
