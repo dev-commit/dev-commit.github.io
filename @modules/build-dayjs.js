3846740ecbc935978901cfb51fd2eec5d84b4d76
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2021-11-23 10:41:15 +0300

    bash script

---
+++ @modules/build-dayjs.js
@@ -0,0 +1,23 @@
+root = 'dayjs/';
+
+ARR_MODULE = [
+    {
+        title: 'Основное',
+        themes: [
+            [root+'main/info',       emoji.theory+'Информация'],
+            [root+'main/variants',   emoji.code+'Варианты'],
+        ]
+    },
+    {
+        title: 'Методы',
+        themes: [
+            [root+'methods/format',     setTheme(1, ['format'])],
+        ]
+    },
+    {
+        title: 'Прочее',
+        themes: [
+            [root+'other/typescript', 'TypeScript'],
+        ]
+    },
+]
---
+++ @modules/build-react-final-form.js
@@ -0,0 +1,24 @@
+root = 'react-final-form/';
+
+ARR_MODULE = [
+    {
+        title: 'Основы',
+        themes: [
+            [root+'main/info',    emoji.theory+'Информация'],
+        ]
+    },
+    {
+        title: 'API',
+        themes: [
+            [root+'api/FormSpy',  setTheme(1, ['&lt;FormSpy /&gt;'])],
+            [root+'api/Field',    setTheme(1, ['&lt;Field /&gt;'])],
+        ]
+    },
+    {
+        title: 'Примеры',
+        themes: [
+            [root+'samples/simple',    'Простой пример'],
+            [root+'samples/type-form', 'Тип для &lt;Form /&gt;'],
+        ]
+    },
+]
