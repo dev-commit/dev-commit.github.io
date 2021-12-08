6b102f80ef2c4b4186305d9f329305b643be253f
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2021-12-08 17:47:41 +0300

    bash script

---
+++ @modules/build-react-query.js
@@ -0,0 +1,17 @@
+root = 'react-query/';
+
+ARR_MODULE = [
+    {
+        title: 'Основы',
+        themes: [
+            [root+'main/info',  emoji.theory+'Информация'],
+        ]
+    },
+    {
+        title: 'Варианты',
+        themes: [
+            [root+'variants/basic',                 emoji.code+'Базовый пример'],
+            [root+'variants/react-query-devtools',  emoji.code+'ReactQuery Devtools'],
+        ]
+    },
+]
---
+++ @modules/build-react-use.js
@@ -0,0 +1,16 @@
+root = 'react-use/';
+
+ARR_MODULE = [
+    {
+        title: 'Основы',
+        themes: [
+            [root+'main/info',  emoji.theory+'Информация'],
+        ]
+    },
+    {
+        title: 'Хуки',
+        themes: [
+            [root+'hooks/useToggle', setTheme(1, ['useToggle'])],
+        ]
+    },
+]
