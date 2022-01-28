ac716e548d42f7a14b5e43ef813a170a0282bb82
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2022-01-29 00:43:42 +0300

    bash script

--- @modules/build-react-router.js
+++ @modules/build-react-router.js
@@ -2,33 +2,51 @@ root = 'react-router/';
 
 ARR_MODULE = [
     {
-        title: 'Основное',
+        title: 'v6',
+        accent: true,
         themes: [
-            [root+'main/info', emoji.theory+'Информация'],
+            [root+'v6/main/info',      emoji.theory+'Информация'],
+            [root+'v6/main/migration', emoji.theory+'Миграция с v5'],
+            [root+'v6/main/install',   emoji.code+'Установка и настройка'],
+            [root+'v6/main/pattern',   emoji.fire+'Шаблон'],
         ]
     },
     {
         title: 'API',
         themes: [
-            [root+'api/Link',                   emoji.chevron+setTheme(1, ['&lt;Link /&gt;', '&lt;NavLink /&gt;'])],
-            [root+'api/BrowserRouter',          emoji.chevron+setTheme(1, ['&lt;BrowserRouter /&gt;'])],
-            [root+'api/Switch',                 emoji.chevron+setTheme(1, ['&lt;Switch /&gt;'])],
-            [root+'api/Route',                  emoji.chevron+setTheme(1, ['&lt;Route /&gt;'])],
+            [root+'v6/api/BrowserRouter',  setTheme(1, ['&lt;BrowserRouter /&gt;'])],
+            [root+'v6/api/Link',           setTheme(1, ['&lt;Link /&gt;'])],
+            [root+'v6/api/NavLink',        setTheme(1, ['&lt;NavLink /&gt;'])],
+            [root+'v6/api/Routes',         setTheme(1, ['&lt;Routes /&gt;'])],
+            [root+'v6/api/Route',          setTheme(1, ['&lt;Route /&gt;'])],
+            [root+'v6/api/useNavigate',    setTheme(3, ['useNavigate()'])],
+            [root+'v6/api/useParams',      setTheme(3, ['useParams()'])],
+        ]
+    },
+
+    //---------------------------------------------------------------------------------------------------------
+
+    {
+        title: 'v5',
+        accent: true,
+        themes: [
+            [root+'v5/main/info',     emoji.theory+'Информация'],
+            [root+'v5/main/install',  emoji.code+'Установка и настройка'],
+            [root+'v5/main/pattern',  emoji.fire+'Шаблон'],
+        ]
+    },
+    {
+        title: 'API',
+        themes: [
+            [root+'v5/api/Switch',  setTheme(1, ['&lt;Switch /&gt;'])],
         ]
     },
     {
         title: 'Варианты',
         themes: [
-            [root+'variants/pattern',           emoji.code+'Шаблон'],
-            [root+'variants/url-parameters',    emoji.pizza+'URL Parameters'],
-            [root+'variants/query-parameters',  emoji.pizza+'Query Parameters'],
-            [root+'variants/page-404',          emoji.pizza+'Page 404'],
-        ]
-    },
-    {
-        title: 'Пакеты',
-        themes: [
-            [root+'packages/react-router', setTheme(2, ['react-router', 'react-router-dom'])],
+            [root+'v5/variants/url-parameters',    emoji.pizza+'URL Parameters'],
+            [root+'v5/variants/query-parameters',  emoji.pizza+'Query Parameters'],
+            [root+'v5/variants/page-404',          emoji.pizza+'Page 404'],
         ]
     },
 ]
