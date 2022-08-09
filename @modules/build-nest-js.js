c37f174bb7ca4c697a0dde793b3a06b144001c8c
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2022-08-09 18:56:48 +0300

    bash script

--- @modules/build-nest-js.js
+++ @modules/build-nest-js.js
@@ -4,9 +4,27 @@ ARR_MODULE = [
     {
         title: 'Основы',
         themes: [
-            [root+'main/info',         emoji.theory+'Информация'],
-            // [root+'main/install',      emoji.code+'Установка и настройка'],
-            // [root+'main/basic-sample', emoji.fire+'Базовый пример'],
+            [root+'main/info',     emoji.theory+'Информация'],
+            [root+'main/install',  emoji.code+'Установка и настройка'],
+            [root+'main/cli',      emoji.code+'Nest CLI'],
+        ]
+    },
+    {
+        title: '1. Controllers',
+        accent: true,
+        themes: [
+            [root+'controllers/structure',    emoji.star+'Структура'],
+            [root+'controllers/http-methods', 'HTTP-методы'],
+            [root+'controllers/url-params',   'Динамические параметры в URL'],
+            [root+'controllers/redirect',     'Редирект'],
+            [root+'controllers/request',      'Request и Response'],
+        ]
+    },
+    {
+        title: '2. Services',
+        accent: true,
+        themes: [
+            [root+'services/structure',    emoji.star+'Структура'],
         ]
     },
 ]
