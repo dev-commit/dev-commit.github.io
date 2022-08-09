06215d5af65522c5816ddf349f27b72abc870bff
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2022-08-09 10:09:27 +0300

    bash script

---
+++ @modules/build-react-native.js
@@ -0,0 +1,12 @@
+root = 'react-native/';
+
+ARR_MODULE = [
+    {
+        title: 'Основы',
+        themes: [
+            [root+'main/info',         emoji.theory+'Информация'],
+            // [root+'main/install',      emoji.code+'Установка и настройка'],
+            // [root+'main/basic-sample', emoji.fire+'Базовый пример'],
+        ]
+    },
+]
