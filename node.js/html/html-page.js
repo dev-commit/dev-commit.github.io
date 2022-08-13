17271a687cf96042ee02b94851dbf72b4766b3fb
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2022-08-13 22:16:46 +0300

    bash script

--- node.js/html/html-page.js
+++ node.js/html/html-page.js
@@ -30,6 +30,7 @@ export const getIndexHTML = ({ up, title, script, menu, content }) => (`
             <title>${title}</title>
             <meta charset="UTF-8">
             <link rel="icon" type="image/png" href="${up}src/img/favicon.svg">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
             <script src="${up}src/js/utils.js"></script>
             <!-- Own JavaScript -->
