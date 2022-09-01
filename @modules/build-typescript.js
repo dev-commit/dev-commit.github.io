afcd5cb2ec7f7b97941f669a30026fdb7fdd2af8
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2022-09-01 09:49:13 +0300

    bash script

--- @modules/build-typescript.js
+++ @modules/build-typescript.js
@@ -12,28 +12,18 @@ ARR_MODULE = [
     {
         title: 'Типы данных',
         themes: [
-            [root+'types/types',   emoji.fire+'Типы данных'],
-            [root+'types/basic',   setTheme(1, ['number', 'string', '...'])],
-            [root+'types/typle',   setTheme(1, ['typle'])],
-            [root+'types/any',     setTheme(1, ['any'])],
-            [root+'types/never',   setTheme(1, ['never'])],
-        ]
-    },
-    {
-        title: 'Операторы',
-        themes: [
-            [root+'operators/keyof',    setTheme(5, ['keyof'])],
-            [root+'operators/typeof',   setTheme(5, ['typeof'])],
-        ]
-    },
-    {
-        title: 'Методы для Type',
-        themes: [
-            [root+'methods/pick',     setTheme(5, ['Pick'])],
-            [root+'methods/exclude',  setTheme(5, ['Exclude'])],
-            [root+'methods/record',   setTheme(5, ['Record'])],
-            [root+'methods/partial',  setTheme(5, ['Partial'])],
-            [root+'methods/omit',     setTheme(5, ['Omit'])],
+            [root+'types/types',          emoji.fire+'Типы данных'],
+            [root+'types/any-vs-unknown', '<em>any</em> vs <em>unknown</em>'],
+
+            [root+'types/string',         setTheme(1, ['string'])],
+            [root+'types/boolean',        setTheme(1, ['noolean'])],
+            [root+'types/number',         setTheme(1, ['number'])],
+            [root+'types/array',          setTheme(1, ['array'])],
+            [root+'types/object',         setTheme(1, ['object'])],
+            [root+'types/null',           setTheme(1, ['null', 'undefined'])],
+            [root+'types/typle',          setTheme(1, ['typle'])],
+            [root+'types/any',            setTheme(1, ['any'])],
+            [root+'types/never',          setTheme(1, ['never'])],
         ]
     },
     {
@@ -92,24 +82,43 @@ ARR_MODULE = [
         ]
     },
     {
-        title: 'React',
+        title: 'Операторы',
+        accent: true,
         themes: [
-            [root+'react/props',         setTheme(3, ['Props'])],
-            [root+'react/types',         'Типы'],
-            [root+'react/samples-types', 'Примеры типизации'],
+            [root+'operators/keyof',                   setTheme(5, ['keyof'])],
+            [root+'operators/typeof',                  setTheme(5, ['typeof'])],
+            [root+'operators/symbol-exclamation-mark', setTheme(5, ['!'])],
         ]
     },
     {
-        title: 'Примеры',
+        title: 'Методы для Type',
+        accent: true,
         themes: [
-            [root+'samples/samples', 'Примеры'],
+            [root+'methods/pick',     setTheme(5, ['Pick'])],
+            [root+'methods/exclude',  setTheme(5, ['Exclude'])],
+            [root+'methods/record',   setTheme(5, ['Record'])],
+            [root+'methods/partial',  setTheme(5, ['Partial'])],
+            [root+'methods/omit',     setTheme(5, ['Omit'])],
+            [root+'methods/merge',    setTheme(5, ['Merge'])],
+            [root+'methods/required', setTheme(5, ['Required'])],
+        ]
+    },
+    {
+        title: 'React',
+        themes: [
+            [root+'react/component', setTheme(3, ['Component'])],
+            [root+'react/useState',  setTheme(3, ['useState'])],
+            [root+'react/useRef',    setTheme(3, ['useRef'])],
+            [root+'react/children',  setTheme(3, ['Children'])],
+            [root+'react/samples',   emoji.code+'Примеры'],
         ]
     },
     {
         title: 'Прочее',
         themes: [
-            [root+'other/guards', 'Guards'],
-            [root+'other/ide',    'IDE'],
+            [root+'other/samples',  emoji.code+'Примеры'],
+            [root+'other/guards',  'Guards'],
+            [root+'other/ide',     'IDE'],
         ]
     },
 ]
