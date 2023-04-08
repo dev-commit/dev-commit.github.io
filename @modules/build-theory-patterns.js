0e569eb09b662a4c6d95b2a51047e8176712f23d
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-04-08 20:28:54 +0400

    bash script

--- @modules/build-theory-patterns.js
+++ @modules/build-theory-patterns.js
@@ -8,31 +8,34 @@ ARR_MODULE = [
         ]
     },
     {
+        // Creational Design Patterns
         title: '1. Порождающие',
         themes: [
-            [root+'patterns-generating/singleton',      emoji.pizza+setTheme(4, ['«Синглтон»'])],
-            [root+'patterns-generating/builder',        emoji.pizza+setTheme(4, ['«Строитель»'])],
-            [root+'patterns-generating/simple-factory', emoji.pizza+setTheme(4, ['«Простая фабрика»'])],
+            [root+'1-creational/singleton',      setTheme(5, ['Singleton'])],
+            [root+'1-creational/builder',        setTheme(5, ['Builder'])],
+            [root+'1-creational/simple-factory', setTheme(4, ['Простая фабрика'])],
         ]
     },
     {
+        // Structural Design Patterns
         title: '2. Структурные',
         themes: [
-            [root+'patterns-structural/decorator', emoji.pizza+setTheme(4, ['«Декоратор»'])],
-            [root+'patterns-structural/facade',    emoji.pizza+setTheme(4, ['«Фасад»'])],
+            [root+'2-structural/decorator', setTheme(5, ['Decorator'])],
+            [root+'2-structural/facade',    setTheme(5, ['Facade'])],
         ]
     },
     {
+        // Behavioral Design Patterns
         title: '3. Поведенческие',
         themes: [
-            [root+'patterns-behavioral/observer', emoji.pizza+setTheme(4, ['«Наблюдатель»'])],
+            [root+'3-behavioral/observer', up+setTheme(5, ['Observer'])],
         ]
     },
     {
         title: '4. Неизвестно',
         themes: [
-            [root+'patterns-other/module',           emoji.pizza+setTheme(4, ['«Модуль»'])],
-            [root+'patterns-other/module-revealing', emoji.pizza+setTheme(4, ['«Открытый модуль»'])],
+            [root+'patterns-other/module',           setTheme(4, ['Модуль'])],
+            [root+'patterns-other/module-revealing', setTheme(4, ['Открытый модуль'])],
         ]
     },
 ]
