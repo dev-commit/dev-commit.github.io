43ac74bf8a17f7061d196eb9b5f4a7d374bee05d
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-04-19 22:30:21 +0400

    bash script

--- @modules/build-theory-network.js
+++ @modules/build-theory-network.js
@@ -15,8 +15,8 @@ ARR_MODULE = [
             [root+'http/http2',               emoji.fire+'HTTP/2'],
             [root+'http/https',               emoji.fire+'HTTPS'],
             [root+'http/1-http-structure',    emoji.star+'1. Структура протокола'],
-            [root+'http/2-http-headers',      emoji.star+'2. HTTP Headers'],
-            [root+'http/3-http-methods',      emoji.star+'3. HTTP Methods'],
+            [root+'http/2-http-methods',      emoji.star+'2. HTTP Methods'],
+            [root+'http/3-http-headers',      emoji.star+'3. HTTP Headers'],
             [root+'http/4-http-status-codes', emoji.star+'4. HTTP Status Codes'],
         ]
     },
