57f56ca2a53bae3d3ab64f3a7aa0e1018c241a90
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-04-27 18:41:45 +0400

    bash script

--- @themes/theory-design-patterns/build-theory-design-patterns.js
+++ @themes/theory-design-patterns/build-theory-design-patterns.js
@@ -11,31 +11,34 @@ ARR_MODULE = [
         title: '1. Creational (Порождающие)',
         selected: true,
         themes: [
-            [root+'1-creational/singleton',      up+setTheme(5, ['Singleton'])],
-            [root+'1-creational/builder',        setTheme(5, ['Builder'])],
-            [root+'1-creational/simple-factory', setTheme(4, ['Простая фабрика'])],
+            [root+'1-creational/singleton',    up+'Singleton'],
+            [root+'1-creational/constructor',  'Constructor'],
+            [root+'1-creational/factory',      'Factory'],
+            [root+'1-creational/prototype',    'Prototype'],
+            [root+'1-creational/builder',      'Builder'],
         ]
     },
     {
         title: '2. Structural (Структурные)',
         selected: true,
         themes: [
-            [root+'2-structural/decorator', setTheme(5, ['Decorator'])],
-            [root+'2-structural/facade',    setTheme(5, ['Facade'])],
+            [root+'2-structural/adapter',   'Adapter'],
+            [root+'2-structural/decorator', 'Decorator'],
+            [root+'2-structural/facade',    'Facade'],
         ]
     },
     {
         title: '3. Behavioral (Поведенческие)',
         selected: true,
         themes: [
-            [root+'3-behavioral/observer', up+setTheme(5, ['Observer'])],
+            [root+'3-behavioral/observer', up+'Observer'],
         ]
     },
     {
         title: '4. Неизвестно',
         themes: [
-            [root+'patterns-other/module',           setTheme(4, ['Модуль'])],
-            [root+'patterns-other/module-revealing', setTheme(4, ['Открытый модуль'])],
+            [root+'patterns-other/module',           'Модуль'],
+            [root+'patterns-other/module-revealing', 'Открытый модуль'],
         ]
     },
 ]
