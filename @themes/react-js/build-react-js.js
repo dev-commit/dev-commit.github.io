07f782c0ba330a81e9536a4887d6eeb92b525021
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-05-19 11:59:27 +0400

    bash script

--- @themes/react-js/build-react-js.js
+++ @themes/react-js/build-react-js.js
@@ -1,21 +1,20 @@
 root = 'react-js/';
 
 ARR_MODULE = [
-    // TODO SUB-TITLE: React.js
     {
         title: 'Реконсиляция',
         themes: [
             [root+'reconciler/virtual-dom',        emoji.theory+'Virtual DOM (Общее)'],
-            [root+'reconciler/virtual-dom-react',  emoji.lemon+'Virtual DOM (React)'],
-            [root+'reconciler/reconciliation',     emoji.lemon+'Reconciliation Algorithm'],
+            [root+'reconciler/virtual-dom-react',  emoji.theory+'Virtual DOM (React)'],
+            [root+'reconciler/reconciliation',     emoji.theory+'Reconciliation Algorithm'],
         ]
     },
     {
         title: 'Fiber',
         themes: [
             [root+'fiber/common',     emoji.star+'Общее'],
-            [root+'fiber/planning',   emoji.star+'1. Планирование'],
-            [root+'fiber/priorities', emoji.star+'2. Приоритизация'],
+            [root+'fiber/planning',   emoji.label+'1. Планирование'],
+            [root+'fiber/priorities', emoji.label+'2. Приоритизация'],
             [root+'fiber/sample',     'Пример'],
         ]
     },
@@ -23,70 +22,79 @@ ARR_MODULE = [
         title: 'Информация',
         themes: [
             [root+'info/info',        emoji.theory+'Информация'],
+            [root+'info/version',     emoji.theory+'Версии'],
             [root+'info/patterns',    emoji.theory+'Шаблоны проектирования'],
-            [root+'info/structure',   emoji.code+'Структура'],
-            [root+'info/components',  emoji.cookie+'Компоненты'],
-            [root+'info/props',       emoji.cookie+'Пропсы'],
+            [root+'info/components',  emoji.theory+'Компоненты'],
+            [root+'info/structure',   emoji.code+'Структура на Class'],
         ]
     },
     {
         title: 'API',
         themes: [
-            [root+'api/jsx',        emoji.boom+'JSX'],
-            [root+'api/children',   emoji.boom+'Children'],
-            [root+'api/context',    emoji.boom+'Context API'],
-            [root+'api/refs',       emoji.boom+'Рефы'],
-            [root+'api/fragments',  emoji.boom+'Фрагменты'],
-            [root+'api/portals',    emoji.boom+'Порталы'],
+            [root+'api/jsx',             'JSX'],
+            [root+'api/children',        setTheme(1, ['props.children'])],
+            [root+'api/props',           setTheme(1, ['props'])],  
+        ]
+    },
+    {
+        title: 'React',
+        child: true,
+        themes: [
+            [root+'api/createContext',   setTheme(1, ['createContext'])],
+            [root+'api/createRef',       setTheme(1, ['createRef'])],
+            [root+'api/react-fragment',  setTheme(1, ['Fragment'])],
+            [root+'api/react-component', setTheme(1, ['Component'])],
+        ]
+    },
+    {
+        title: 'ReactDOM',
+        child: true,
+        themes: [
+            [root+'api/createPortal',    setTheme(1, ['createPortal'])],
         ]
     },
     {
         title: 'Паттерны',
         themes: [
-            [root+'patterns/higher-order-components',  emoji.donut+'Higher-Order Component'],
-            [root+'patterns/render-props',             emoji.donut+'Render Props'],
-            [root+'patterns/element-props',            emoji.donut+'Свойства-элементы'],
-            [root+'patterns/lifting-state-up',         emoji.donut+'Подъём состояния'],
+            [root+'patterns/higher-order-components',  'Higher-Order Component'],
+            [root+'patterns/render-props',             'Render Props'],
+            [root+'patterns/element-props',            'Свойства-элементы'],
+            [root+'patterns/lifting-state-up',         'Подъём состояния'],
         ]
     },
     {
         title: 'Обработка и вывод данных',
         themes: [
-            [root+'processing-data-output/handling-events',          emoji.cookie+'Обработка событий'],
-            [root+'processing-data-output/controlled-components',    emoji.cookie+'Controlled Components'],
-            [root+'processing-data-output/uncontrolled-components',  emoji.cookie+'Uncontrolled Components'],
-            [root+'processing-data-output/css',                      emoji.cookie+'CSS'],
-            [root+'processing-data-output/conditional-rendering',    emoji.cookie+'Условный рендеринг'],
-            [root+'processing-data-output/lists-and-keys',           emoji.cookie+'Списки и ключи'],
-            [root+'processing-data-output/error-boundaries',         emoji.cookie+'Предохранители'],
+            [root+'processing-data-output/handling-events',          'Обработка событий'],
+            [root+'processing-data-output/controlled-components',    'Controlled Components'],
+            [root+'processing-data-output/uncontrolled-components',  'Uncontrolled Components'],
+            [root+'processing-data-output/css',                      'CSS'],
+            [root+'processing-data-output/conditional-rendering',    'Условный рендеринг'],
+            [root+'processing-data-output/lists-and-keys',           'Списки и ключи'],
+            [root+'processing-data-output/error-boundaries',         'Предохранители'],
         ]
     },
     {
         title: 'Жизненный цикл компонентов',
         themes: [
             [root+'lifecycle/info',                               emoji.theory+'Информация'],
-            ['', ''],
-            [root+'lifecycle/hook-constructor',                   setTheme(1, ['constructor()'])],
-            [root+'lifecycle/hook-get-derived-state-from-props',  setTheme(1, ['getDerivedStateFromProps()'])],
-            [root+'lifecycle/hook-should-component-update',       setTheme(1, ['shouldComponentUpdate()'])],
-            [root+'lifecycle/hook-render',                        setTheme(1, ['render()'])],
-            [root+'lifecycle/hook-get-snapshot-before-update',    setTheme(1, ['getSnapshotBeforeUpdate()'])],
-            ['', ''],
-            [root+'lifecycle/hook-component-did-mount',           setTheme(3, ['componentDidMount()'])],
-            [root+'lifecycle/hook-component-did-update',          setTheme(3, ['componentDidUpdate()'])],
-            [root+'lifecycle/hook-component-will-unmount',        setTheme(3, ['componentWillUnmount()'])],
-            ['', ''],
-            [root+'lifecycle/hook-get-derived-state-from-error',  setTheme(1, ['getDerivedStateFromError()'])],
-            [root+'lifecycle/hook-component-did-catch',           setTheme(1, ['componentDidCatch()'])],
+            [root+'lifecycle/hook-constructor',                   setTheme(1, ['constructor'])],
+            [root+'lifecycle/hook-get-derived-state-from-props',  setTheme(1, ['getDerivedStateFromProps'])],
+            [root+'lifecycle/hook-should-component-update',       setTheme(1, ['shouldComponentUpdate'])],
+            [root+'lifecycle/hook-render',                        setTheme(1, ['render'])],
+            [root+'lifecycle/hook-get-snapshot-before-update',    setTheme(1, ['getSnapshotBeforeUpdate'])],
+            [root+'lifecycle/hook-component-did-mount',           setTheme(3, ['componentDidMount'])],
+            [root+'lifecycle/hook-component-did-update',          setTheme(3, ['componentDidUpdate'])],
+            [root+'lifecycle/hook-component-will-unmount',        setTheme(3, ['componentWillUnmount'])],
+            [root+'lifecycle/hook-get-derived-state-from-error',  setTheme(6, ['getDerivedStateFromError'])],
+            [root+'lifecycle/hook-component-did-catch',           setTheme(6, ['componentDidCatch'])],
         ]
     },
-    // TODO SUB-TITLE: React.js-Остальное
     {
         title: 'Прочее',
         themes: [
             [root+'other/react-developer-tools',  'React Developer Tools'],
-            [root+'other/components',             'Компоненты'],
-            [root+'other/samples',                'Примеры'],
+            [root+'other/samples',                emoji.code+'Примеры'],
         ]
     },
     {
