97c05b13e510b4cbcce26de240ad3cbc98e7e526
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-06-06 17:27:17 +0400

    bash script

--- @themes/theory-solid/build-theory-solid.js
+++ @themes/theory-solid/build-theory-solid.js
@@ -4,14 +4,14 @@ ARR_MODULE = [
     {
         title: 'Основы',
         themes: [
-            getLink(root, 'info'),
+            getLink(root, 'info', true),
         ]
     },
     {
         title: 'Principles',
         accent: true,
         themes: [
-            [root+'principles/1-srp',  '1. Single Responsibility'],
+            [root+'principles/1-srp',  '1. Single Responsibility', false, 'done'],
             [root+'principles/2-ocp',  '2. Open Closed'],
             [root+'principles/3-lsp',  '3. Liskov Substitution'],
             [root+'principles/4-isp',  '4. Interface Segregation'],
