72df5bc06d25ec0f6e6bb0974c0b9f6274a6bb9b
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-08-04 19:11:26 +0400

    bash script

--- @themes/auth/build-auth.js
+++ @themes/auth/build-auth.js
@@ -2,16 +2,24 @@ root = 'auth/';
 
 ARR_MODULE = [
     {
+        title: 'Session',
+        themes: [
+            [root+'session/session', 'Session & Cookie'],
+        ]
+    },
+    {
         title: 'JWT',
         themes: [
-            [root+'jwt/info', emoji.theory+'Информация'],
+            [root+'jwt/info',      emoji.theory+'Информация'],
+            [root+'jwt/algoritm', 'Алгоритм работы'],
+            [root+'jwt/tokens',   'Токены'],
         ]
     },
     {
         title: 'Сервисы',
         themes: [
             [root+'services/auth0', setTheme(1, ['auth0'])],
-            [root+'services/okta', setTheme(1, ['okta'])],
+            [root+'services/okta',  setTheme(1, ['okta'])],
         ]
     },
 ]
