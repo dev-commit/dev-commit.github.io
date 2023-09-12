13413209e35a2ced6b349e3e74c3b907b57f0b3f
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-09-13 00:18:30 +0400

    bash script

--- @themes/ide/build-ide.js
+++ @themes/ide/build-ide.js
@@ -4,26 +4,28 @@ ARR_MODULE = [
     {
         title: 'VSCode',
         themes: [
-            [root+'vscode/info',              emoji.theory+'Информация'],
-            [root+'vscode/plugins',           emoji.code+'Плагины'],
-            [root+'vscode/snippets',          'Сниппеты'],
-            [root+'vscode/settings',          'Настройки'],
-            [root+'vscode/shortcuts',         'Shortcuts'],
+            [root+'vscode/info',      emoji.theory+'Информация'],
+            [root+'vscode/plugins',   emoji.code+'Плагины'],
+            [root+'vscode/snippets',  'Сниппеты'],
+            [root+'vscode/settings',  'Настройки'],
+            [root+'vscode/shortcuts', 'Shortcuts'],
         ]
     },
     {
         title: 'Редакторы код',
         themes: [
-            [root+'editors/sublime-text',  'Sublime Text'],
-            [root+'editors/atom',          'Atom'],
-            [root+'editors/notepad',       'Notepad++'],
-            [root+'editors/codesandbox',   'CodeSandbox'],
+            [root+'editors/codesandbox',  'CodeSandbox'],
+            [root+'editors/sublime-text', 'Sublime Text'],
+            [root+'editors/notepad',      'Notepad++'],
+            [root+'editors/atom',         'Atom'],
         ]
     },
+
+    
     {
         title: 'Плагины',
         themes: [
-            [root+'plugins/emmet',  'Emmet'],
+            [root+'plugins/emmet', 'Emmet'],
         ]
     },
 ];
