fdf54a4042adc3aced819ff6f8aea1377fbb9c4b
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-09-13 13:37:42 +0400

    bash script

--- @themes/linters/build-linters.js
+++ @themes/linters/build-linters.js
@@ -5,8 +5,23 @@ ARR_MODULE = [
         title: 'Основы',
         themes: [
             getLink(root, 'install'),
-            [root+'main/ESLint', 'ESLint'],
-            [root+'main/Prettier', 'Prettier'],
+        ]
+    },
+    {
+        title: 'Prettier',
+        themes: [
+            [root+'prettier/info',           emoji.theory+'Информация'],
+            [root+'prettier/vscode-plugin',  'Плагин для VSCode'],
+            [root+'prettier/prettierrc',     setTheme(5, ['.prettierrc.json'])],
+            [root+'prettier/prettierignore', setTheme(5, ['.prettierignore'])],
+        ]
+    },
+    {
+        title: 'ESLint',
+        themes: [
+            [root+'eslint/info',    emoji.theory+'Информация'],
+            [root+'eslint/main',    'Основное'],
+            [root+'eslint/disable', 'Disable'],
         ]
     },
 ]
