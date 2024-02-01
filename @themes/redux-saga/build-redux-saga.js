3622e01f571439c9d7ce8f9954ebfd1b3f9d3656
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-02-01 16:04:05 +0400

    Bash Script Commit

--- @themes/redux-saga/build-redux-saga.js
+++ @themes/redux-saga/build-redux-saga.js
@@ -8,10 +8,26 @@ ARR_MODULE = [
             getLink(root, 'install'),
         ]
     },
+
+    {
+        title: 'Методы',
+        themes: [
+            [root+'methods/call', setTheme(1, ['call'])],
+            [root+'methods/put', setTheme(1, ['put'])],
+            [root+'methods/take', setTheme(1, ['take'])],
+            [root+'methods/all', setTheme(1, ['all'])],
+            [root+'methods/delay', setTheme(1, ['delay'])],
+            [root+'methods/fork', setTheme(1, ['fork'])],
+            [root+'methods/select', setTheme(1, ['select'])],
+            [root+'methods/cancel', setTheme(1, ['cancel'])],
+            [root+'methods/debounce', setTheme(1, ['debounce'])],
+            [root+'methods/takeEvery', setTheme(1, ['takeEvery'])],
+            [root+'methods/takeLatest', setTheme(1, ['takeLatest'])],
+        ]
+    },
     {
         title: 'Общее',
         themes: [
-            [root+'common/methods', emoji.star+'Методы'],
             [root+'common/accessing-store-value', 'Доступ к значению Store'],
             [root+'common/saga-vs-thunk', 'redux-saga vs redux-thunk'],
             [root+'common/questions', 'Вопросы'],
