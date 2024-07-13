96d20ff7778117bc757a7c442d53e4102336fd60
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-07-13 18:38:29 +0400

    Bash Script Commit

--- @themes/cypress/build-cypress.js
+++ @themes/cypress/build-cypress.js
@@ -12,6 +12,7 @@ ARR_MODULE = [
     {
         title: 'Методы',
         themes: [
+            [root+'methods/methods',  'Общие методы'],
             [root+'methods/should',   setTheme(1, ['should'])],
             [root+'methods/type',     setTheme(1, ['type', 'clear', 'blur'])],
             [root+'methods/visit',    setTheme(1, ['visit', 'pause', 'wait'])],
@@ -33,9 +34,26 @@ ARR_MODULE = [
         ]
     },
     {
+        title: 'Библиотеки',
+        themes: [
+            [root+'libs/cypress-image-snapshot',  '<u-inline-circle>1</u-inline-circle> '+setTheme(2, ['cypress-image-snapshot'])],
+            [root+'libs/mocha-allure-reporter',  '<u-inline-circle>4</u-inline-circle> '+setTheme(2, ['mocha-allure-reporter', 'allure-commandline'])],
+
+            [root+'libs/cypress-mochawesome-reporter',  setTheme(2, ['cypress-mochawesome-reporter'])],
+            [root+'libs/cypress-visual-regression',  setTheme(2, ['cypress-visual-regression'])],
+            [root+'libs/cypress-file-upload',  setTheme(2, ['cypress-file-upload'])],
+        ]
+    },
+    {
         title: 'Guides',
         themes: [
             [root+'guides/network-requests', emoji.star+'Network Requests'],
+        ]
+    },
+    {
+        title: 'Прочее',
+        themes: [
+            [root+'other/trash', 'Свалка'],
         ]
     },
 ]
