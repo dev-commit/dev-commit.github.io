9bd7f6e013a236a00d24e38ce140aab97f7e45e8
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-09-13 12:52:33 +0400

    Bash Script Commit

--- @modules/@links.js
+++ @modules/@links.js
@@ -87,7 +87,7 @@ const ARR_LINKS = [
     { page: 'vue-vuex',                theme: 'white',    title: search+'Vuex' },
     { page: 'vue-router',              theme: 'white',    title: search+'Vue Router' },
     { page: 'vue-cli',                 theme: 'white',    title: search+'Vue CLI' },
-    { page: 'vue-plugins',             theme: 'white',    title: search+'Vue Plugins' },
+    { page: 'vue-plugins',             theme: 'white',    title: 'Vue Plugins' },
     null,
     { page: 'testing',                 theme: 'brown',    title: search+theory+'Testing' },
     { page: 'jest',                    theme: 'brown',    title: search+'Jest' },
