722869124c78faca1b6ab6e3b75827725b014bce
Anton Sergeenkov <asergeenkov@combotech.net>
2025-12-20 16:51:23 +0400

    Bash Script Commit

--- @themes/react-router/build-react-router.js
+++ @themes/react-router/build-react-router.js
@@ -7,20 +7,19 @@ ARR_MODULE = [
         themes: [
             getLink(root, 'info', true),
             getLink(root, 'install', true),
-            getLink(root, 'use', true),
         ]
     },
     {
         title: 'Declarative Mode',
         themes: [
-            [root+'mode-declarative/BrowserRouter', setTheme(1, ['&lt;BrowserRouter /&gt;']), false, 'done'],
-            [root+'mode-declarative/Routes',        setTheme(1, ['&lt;Routes /&gt;', '&lt;Route /&gt;']), false, 'done'],
-            [root+'mode-declarative/Link',          setTheme(1, ['&lt;Link /&gt;', '&lt;NavLink /&gt;']), false, 'done'],
-            [root+'mode-declarative/useNavigate',   setTheme(3, ['useNavigate()']), false, 'done'],
-            [root+'mode-declarative/useParams',     setTheme(3, ['useParams()']), false, 'done'],
-
-            [root+'mode-declarative/useSearchParams',     setTheme(3, ['useSearchParams()'])],
-            [root+'mode-declarative/useLocation',     setTheme(3, ['useLocation()'])],
+            [root+'mode-declarative/use', emoji.fire+'Базовый пример', false, 'done'],
+            [root+'mode-declarative/api/BrowserRouter', setTheme(1, ['&lt;BrowserRouter /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/Routes',        setTheme(1, ['&lt;Routes /&gt;', '&lt;Route /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/Link',          setTheme(1, ['&lt;Link /&gt;', '&lt;NavLink /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/useNavigate',   setTheme(3, ['useNavigate()']), false, 'done'],
+            [root+'mode-declarative/api/useParams',     setTheme(3, ['useParams()']), false, 'done'],
+            [root+'mode-declarative/api/useSearchParams',     setTheme(3, ['useSearchParams()'])],
+            [root+'mode-declarative/api/useLocation',     setTheme(3, ['useLocation()'])],
         ]
     },
     // {
