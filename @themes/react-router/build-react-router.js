ac0591f2d9ec691d31840187a5e677d6685025c4
Anton Sergeenkov <asergeenkov@combotech.net>
2025-12-20 16:51:47 +0400

    Bash Script Commit

--- @themes/react-router/build-react-router.js
+++ @themes/react-router/build-react-router.js
@@ -13,12 +13,13 @@ ARR_MODULE = [
         title: 'Declarative Mode',
         themes: [
             [root+'mode-declarative/use', emoji.fire+'Базовый пример', false, 'done'],
-            [root+'mode-declarative/api/BrowserRouter', setTheme(1, ['&lt;BrowserRouter /&gt;']), false, 'done'],
-            [root+'mode-declarative/api/Routes',        setTheme(1, ['&lt;Routes /&gt;', '&lt;Route /&gt;']), false, 'done'],
-            [root+'mode-declarative/api/Link',          setTheme(1, ['&lt;Link /&gt;', '&lt;NavLink /&gt;']), false, 'done'],
-            [root+'mode-declarative/api/useNavigate',   setTheme(3, ['useNavigate()']), false, 'done'],
-            [root+'mode-declarative/api/useParams',     setTheme(3, ['useParams()']), false, 'done'],
-            [root+'mode-declarative/api/useSearchParams',     setTheme(3, ['useSearchParams()'])],
+
+            [root+'mode-declarative/api/BrowserRouter',   setTheme(1, ['&lt;BrowserRouter /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/Routes',          setTheme(1, ['&lt;Routes /&gt;', '&lt;Route /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/Link',            setTheme(1, ['&lt;Link /&gt;', '&lt;NavLink /&gt;']), false, 'done'],
+            [root+'mode-declarative/api/useNavigate',     setTheme(3, ['useNavigate()']), false, 'done'],
+            [root+'mode-declarative/api/useParams',       setTheme(3, ['useParams()']), false, 'done'],
+            [root+'mode-declarative/api/useSearchParams', setTheme(3, ['useSearchParams()'])],
             [root+'mode-declarative/api/useLocation',     setTheme(3, ['useLocation()'])],
         ]
     },
