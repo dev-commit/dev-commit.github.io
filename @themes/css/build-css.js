9a32e78ffb8343e569d4f500e2d572b8e4cf54f3
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-09 12:38:27 +0400

    Bash Script Commit

--- @themes/css/build-css.js
+++ @themes/css/build-css.js
@@ -2,38 +2,8 @@ root = "css/";
 
 ARR_MODULE = [
   {
-    title: "CSS Color Module",
-    themes: [
-      [
-        root + "color-module/colors-hex",
-        "1. " + setTheme(3, ["Hexadecimal Colors"]),
-      ],
-      [
-        root + "color-module/colors-rgb",
-        "2. " + setTheme(3, ["rgb", "rgba"]),
-        false,
-        "done",
-      ],
-      [
-        root + "color-module/colors-hsl",
-        "3. " + setTheme(3, ["hsl", "hsla"]),
-        false,
-        "done",
-      ],
-      [root + "color-module/colors-hwb", "4. " + setTheme(3, ["HWB"])],
-      [
-        root + "color-module/colors-predefined",
-        "5. " + setTheme(3, ["Predefined"]),
-      ],
-    ],
-  },
-
-  {
     title: "Основное",
-    themes: [
-      [root + "main/custom-properties", "Custom Properties"],
-      [root + "main/samples", "Примеры"],
-    ],
+    themes: [[root + "main/custom-properties", "Custom Properties"]],
   },
   {
     title: "Layout",
@@ -52,24 +22,25 @@ ARR_MODULE = [
     ],
   },
   {
-    title: "Особенности",
-    themes: [
-      [
-        root + "special/collapse-margin",
-        emoji.code + "Схлопывание внешних отступов",
-      ],
-      [
-        root + "special/dropping-out-margin",
-        emoji.code + "Выпадение внешних отступов",
-      ],
-    ],
-  },
-  {
     title: "Features",
     themes: [
       [root + "features/mixins", "@apply"],
       [root + "features/custom-selector", "@custom-selector"],
       [root + "features/environment-variables", "CSS Environment Variables"],
+    ],
+  },
+  {
+    title: "Примеры",
+    themes: [
+      [root + "samples/samples", "Примеры"],
+      [
+        root + "samples/collapse-margin",
+        emoji.code + "Схлопывание внешних отступов",
+      ],
+      [
+        root + "samples/dropping-out-margin",
+        emoji.code + "Выпадение внешних отступов",
+      ],
     ],
   },
 ];
