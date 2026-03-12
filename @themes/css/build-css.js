53ec984ab47127cc644c18bed1141bd2fb3a2c9d
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-12 11:59:01 +0400

    Bash Script Commit

--- @themes/css/build-css.js
+++ @themes/css/build-css.js
@@ -4,7 +4,6 @@ ARR_MODULE = [
   {
     title: "Shapes and Clipping",
     themes: [
-      [root + "graphics/blend-mode", "CSS Blend Modes"],
       [root + "graphics/css-shapes", "CSS Shapes"],
       [root + "graphics/css-masking", "CSS Masking"],
     ],
