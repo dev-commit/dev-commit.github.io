d8b90c1ca31bdde62a82b860f948a3e5e1dfe59f
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-28 01:33:38 +0400

    Bash Script Commit

--- @themes/vue-js/build-vue-js.js
+++ @themes/vue-js/build-vue-js.js
@@ -13,14 +13,4 @@ ARR_MODULE = [
       [root + "main/transition", setTheme(1, ["&lt;transition&gt;"])],
     ],
   },
-  {
-    title: "Директивы",
-    themes: [
-      [root + "directives/v-bind", setTheme(1, ["v-bind:"]) + " :class :style"],
-      [root + "directives/v-model", setTheme(1, ["v-model"])],
-      [root + "directives/v-pre", setTheme(1, ["v-pre"])],
-      [root + "directives/v-cloak", setTheme(1, ["v-cloak"])],
-      [root + "directives/v-once", setTheme(1, ["v-once"])],
-    ],
-  },
 ];
