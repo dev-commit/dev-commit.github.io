0bd33246bb71012c55022a0b05d1508ed08ea0cf
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-28 17:30:54 +0400

    Bash Script Commit

--- @themes/vue-js/build-vue-js.js
+++ @themes/vue-js/build-vue-js.js
@@ -5,7 +5,6 @@ ARR_MODULE = [
     title: "Main",
     accent: true,
     themes: [
-      [root + "main/component-dynamic", "Динамические компоненты"],
       [root + "main/computed", setTheme(1, ["computed"])],
       [root + "main/emit", setTheme(1, ["$emit"])],
       [root + "main/transition", setTheme(1, ["&lt;transition&gt;"])],
