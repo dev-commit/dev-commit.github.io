095873185573868c6b05c24ff53387f770d8030f
Anton Sergeenkov <asergeenkov@combotech.net>
2026-04-11 21:40:53 +0400

    Bash Script Commit

--- @themes/theory-programming/build-theory-programming.js
+++ @themes/theory-programming/build-theory-programming.js
@@ -2,22 +2,6 @@ root = "theory-programming/";
 
 ARR_MODULE = [
   {
-    title: "ООП",
-    themes: [
-      [root + "oop/info", emoji.theory + "Информация"],
-      [root + "oop/access-modifiers", "Модификаторы доступа"],
-    ],
-  },
-  {
-    title: "Принципы ООП",
-    themes: [
-      [root + "oop/principles/encapsulation", emoji.items + "1. Инкапсуляция"],
-      [root + "oop/principles/inheritance", emoji.items + "2. Наследование"],
-      [root + "oop/principles/polymorphism", emoji.items + "3. Полиморфизм"],
-      [root + "oop/principles/abstraction", emoji.items + "4. Абстракция"],
-    ],
-  },
-  {
     title: "ФП",
     themes: [
       [root + "fp/functional", emoji.theory + "Информация"],
